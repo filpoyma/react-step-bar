c60aad991ae27914e613e171d964cfb713de139c
rom <filpoyma@gmail.com>
2021-08-12 14:15:11 +0300

    skeleton

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProgressBar from "./components/ProgressBar";
+
+function App() {
+  return <div className='App'>Hi React
+  <ProgressBar/>
+  </div>;
+}
+
+export default App;
---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './progressBar.module.css';
+
+function ProgressBar(props) {
+  return <div className={styles.error}>Hi, Gys</div>;
+}
+
+export default ProgressBar;
